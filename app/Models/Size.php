5e0b315596abaf76f544556df9597be402a894a7
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-06 21:26:42 +0300

    Update Size.php

--- app/Models/Size.php
+++ app/Models/Size.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Size extends Model
 {
-    protected $hidden = ['id', "created_at", "updated_at"];
+    protected $hidden = ["created_at", "updated_at"];
     use HasFactory;
 }
