0b2296c7babb3bc57fe669ddc96d2f736c17681f
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-07 15:02:34 +0300

    Update Size.php

--- app/Models/Size.php
+++ app/Models/Size.php
@@ -2,15 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Color;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Size extends Model
 {
     protected $hidden = ["created_at", "updated_at"];
     use HasFactory;
-    public function color()
+    public function colors()
     {
-        $this->belongsToMany(Size::class, ColorSize::class);
+       return $this->belongsToMany(Color::class, ColorSize::class);
     }
 }
