edcb633050457b02f6578561dea77190d5e3f7b2
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-14 01:40:35 +0300

    Create ProductConfiguration.php

---
+++ app/Models/ProductConfiguration.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Size;
+use App\Models\Color;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class ProductConfiguration extends Model
+{
+    use HasFactory;
+    protected $table = 'product_configuration';
+    protected $hidden = ["id","color_id","size_id","product_id","created_at", "updated_at"];
+
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function color()
+    {
+        return $this->belongsTo(Color::class, 'color_id');
+    }
+
+    public function size()
+    {
+        return $this->belongsTo(Size::class, 'size_id');
+    }
+}
