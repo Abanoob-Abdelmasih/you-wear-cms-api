8aa70aeffc2d9f6fd1488cc03e4cdf1c04443c0d
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-14 14:43:58 +0300

    Update Size.php

--- app/Models/Size.php
+++ app/Models/Size.php
@@ -9,10 +9,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Size extends Model
 {
-    protected $hidden = ["created_at", "updated_at"];
+    protected $hidden = ["created_at", "updated_at","isActive"];
     use HasFactory;
     public function colors()
     {
-        return $this->belongsToMany(Color::class, ProductConfiguration::class)->withTimestamps()->as('configuration');
+        return $this->belongsToMany(Color::class, ProductConfiguration::class, 'size_id', 'color_id')->withTimestamps()->as('configuration');
     }
 }
