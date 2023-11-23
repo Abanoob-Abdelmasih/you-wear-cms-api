b142ba91f96e465c7b64c0a44cfb7a4236be6790
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-24 01:53:36 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -49,6 +49,7 @@ Route::prefix('/product')->name('product.')->group(function () {
 Route::prefix('/cart')->name('cart.')->group(function () {
     Route::post('addToCart', [CartController::class, 'addToCart']);
     Route::get('showCart/{id}', [CartController::class, 'showCart']);
+    Route::post('removeFromCart', [CartController::class, 'removeFromCart']);
 });
 
 Route::get('/test-piv', [SizeController::class, 'many2ManyExamples']);
