4fb294e1f58a84319dfac985c3abbf00442b3f5c
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-23 18:41:00 +0300

    Update CartController.php

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    public function addToCart(Request $request){
-
+    public function addToCart(Request $request)
+    {
+        $newCartItem = new Cart();
+        $newCartItem->user_id = $request->user_id;
+        $newCartItem->product_configuration_id = $request->product_configuration_id;
+        $newCartItem->qunatity = $request->qunatity;
+        $newCartItem->save();
     }
 }
